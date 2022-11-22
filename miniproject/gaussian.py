6229569009ba545ed578dd9b67252e795a765fad
ridin <1067876850@qq.com>
2022-11-22 10:40:30 -0500

    mini project

---
+++ miniproject/gaussian.py
@@ -0,0 +1,12 @@
+import torch
+
+
+class GaussianNoise:
+
+    def __init__(self, mean=0.0, std=1.0):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        noise = torch.randn(tensor.size()) * self.std + self.mean
+        return tensor + noise
